f6b652d34266ce42ec54623b5123124c05eab39d
Suarez <princess.carmi2004@gmail.com>
2025-07-17 10:40:38 +0800

    Sprite

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const cartPopup = document.querySelector('.cart-popup');
 userInfo.addEventListener('click', () => {
   cartPopup.style.display = cartPopup.style.display === 'block' ? 'none' : 'block';
 });
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
 
     document.querySelectorAll('.add-to-cart').forEach(button => {
         button.addEventListener('click', () => {
@@ -21,7 +23,7 @@ userInfo.addEventListener('click', () => {
             totalCoins += price;
 
             userInfo.textContent = `${totalCoins} PokéCoins | Poké Basket (${basketCount})`;
-
+            
             const li = document.createElement('li');
             li.textContent = `${name} - ${price} PokéCoins`;
             cartItems.appendChild(li);
@@ -44,17 +46,33 @@ pokeball.addEventListener('click', () => {
 });
 
 const toggleBtn = document.getElementById('toggle-character');
-const heroCharacter = document.querySelector('.hero-character');
+const buddyCharacter = document.querySelector('.buddy-character');
+const buddySprite = document.getElementById('buddy-sprite');
 
-// Optional: set the default text when page loads
-toggleBtn.textContent = 'Put Away Buddy';
+let idleToggle = true;
+
+setInterval(() => {
+  if (!buddyCharacter.classList.contains('hidden')) {
+    buddySprite.src = idleToggle ? 'asset/Skye_Idle.gif' : 'asset/Skye_Idle2.gif';
+    idleToggle = !idleToggle;
+  }
+}, 5000);
 
 toggleBtn.addEventListener('click', () => {
-  heroCharacter.classList.toggle('hidden');
-
-  if (heroCharacter.classList.contains('hidden')) {
+  buddyCharacter.classList.toggle('hidden');
+  if (buddyCharacter.classList.contains('hidden')) {
     toggleBtn.textContent = 'Bring Out Buddy';
   } else {
     toggleBtn.textContent = 'Put Away Buddy';
   }
+});
+
+buddySprite.addEventListener('click', () => {
+  console.log('Buddy clicked!');
+
+
+  buddySprite.src = 'asset/Skye_Talking.gif';
+  setTimeout(() => {
+    buddySprite.src = 'asset/Skye_Idle.gif';
+  }, 5000);
 });
